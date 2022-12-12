9184511f0db5ea79ac8ca1dfd227a0282c3c1849
Astemir Eleev <astemireleev@gmail.com>
2022-12-12 12:52:57 -0800

    Initial Commit

---
+++ Intera Utils/Intera_UtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intera_UtilsApp.swift
+//  Intera Utils
+//
+//  Created by Astemir Eleev on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Intera_UtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
